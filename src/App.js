005356678a5c7318f0032ba7612d3e5828b46590
Pascal <pascalokereke18@gmail.com>
2023-06-16 00:20:04 +0100

    pascals react tutorial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+// components is a function that return something in a function which is the JSX templates and we export the app
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App Component</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
